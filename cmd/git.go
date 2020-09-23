8e6544a120a49b5259d0b7487043fb4be8bf816f
Andrej Petras <andrej.petras@capgemini.com>
2020-09-23 16:33:03 +0200

    Fix gitlab brancht name

--- cmd/git.go
+++ cmd/git.go
@@ -156,7 +156,7 @@ func gitBranch() string {
 		}
 	}
 	if isGitLab() {
-		tmp, exists := os.LookupEnv("CI_COMMIT_REF_NAME")
+		tmp, exists := os.LookupEnv("CI_COMMIT_REF_SLUG")
 		if exists {
 			return tmp
 		}
